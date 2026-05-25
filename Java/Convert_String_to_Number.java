a00adeb86c36be7330964714f88d055624b643b4
alexsdeatherage <alexdeatherage4829@gmail.com>
2026-05-24 23:13:13 -0700

    Upload Java and JavaScript Solutions

---
+++ Java/Convert_String_to_Number.java
@@ -0,0 +1,11 @@
+// Input: "0" -> Output: 0 
+// Input: "2" -> Output: 2 
+// Input: "-2" -> Output: -2 
+
+public class StringToNumber {
+  public static int stringToNumber(String str) {
+    // take str variable, and turn it into a number using a built-int function
+    return Integer.parseInt(str);
+    // assign the new number to a variable, and return it
+  }
+}
